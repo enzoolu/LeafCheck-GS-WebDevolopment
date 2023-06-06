c417b3b0b6aa7ed64fa92d516a19189a6e488e1f
enzo goulart <enzoo.lu@gmail.com>
2023-06-06 03:04:31 -0300

    adicionando hora

--- assets/JS/inserirData.js
+++ assets/JS/inserirData.js
@@ -1,5 +1,7 @@
-let inserirData = document.getElementById('inserirData')
-let now = new Date()
-    inserirData.innerHTML = ("Hoje é: " + now.getDate().toString().padStart(2,'0') +
-                     "/" + (String(now.getMonth() + 1).padStart(2,'0') +
-                     "/" + now.getFullYear()));
+let inserirData = document.getElementById('inserirData');
+let inserirHora = document.getElementById('inserirHora');
+
+let now = new Date();
+
+inserirData.innerHTML = `Dia: ${now.getDate().toString().padStart(2,'0')}/${String(now.getMonth() + 1).padStart(2,'0')}/${now.getFullYear()}`;
+inserirHora.innerHTML = `Hora: ${now.getHours().toString().padStart(2,'0')}:${now.getMinutes().toString().padStart(2,'0')}`;
